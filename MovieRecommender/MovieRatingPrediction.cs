883e7639e50fc1a868945d61ac40ff6f0834d6de
Hendrick Rafael <inghendrickrafael@gmail.com>
2020-08-04 17:24:47 -0400

    proyecto movie recomender 1.0

---
+++ MovieRecommender/MovieRatingPrediction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRecommender
+{
+    class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
